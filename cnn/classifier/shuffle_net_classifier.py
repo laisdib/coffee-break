5f43b2db6a0f31b0f8e3ded4d708366e37ae830b
Laís Dib Albuquerque <lda.snf20@uea.edu.br>
2023-07-09 19:50:49 -0400

    feat: adding decode_base64 function and refactoring predict funtions

--- cnn/classifier/shuffle_net_classifier.py
+++ cnn/classifier/shuffle_net_classifier.py
@@ -1,21 +1,38 @@
+import base64
+import io
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 
 
-model = tf.keras.models.load_model("ShuffleNet.h5")
+def decode_base64(base64_string):
+    image_data = base64.b64decode(base64_string)
+    image = io.BytesIO(image_data)
+    return image
 
-image_dir = "test_images/test.jpg"
-image = Image.open(image_dir)
-image = image.resize((128, 128))
-image = np.array(image) / 255.
-image = np.expand_dims(image, axis=0)
 
-prediction = model.predict(image)
-class_index = np.argmax(prediction)
-class_probability = prediction[0][class_index] * 100
+def load_image(image):
+    image = Image.open(image)
+    image = image.resize((128, 128))
+    image = np.array(image) / 255.
+    image = np.expand_dims(image, axis=0)
 
-class_names = ["Cercospirose", "Saudável", "Ferrugem", "Bicho Mineiro", "Phoma"]
+    return image
 
-print("Prediction")
-print(f"{class_names[class_index]}: {class_probability}")
+
+def load_cnn_model():
+    model = tf.keras.models.load_model("ShuffleNet.h5")
+    return model
+
+
+def predict_image_class(image, model):
+    class_names = ["Cercospirose", "Saudável", "Ferrugem", "Bicho Mineiro", "Phoma"]
+    prediction = model.predict(image)
+
+    predictions_probability = {key: round(value * 100, 1) for key, value in zip(class_names, prediction[0])}
+    predictions_probability = {key: predictions_probability[key] 
+                               for key in sorted(predictions_probability, 
+                                                 key=predictions_probability.get, 
+                                                 reverse=True)}
+    
+    return predictions_probability
